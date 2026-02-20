3f2a612c0de1a123440ff2ae9125c340001da63f
vikumfernando <it23595026@my.sliit.lk>
2026-02-20 16:59:13 +0530

    Web server created

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+//(1) Reading a file
 const fs = require("fs");
 fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
 
+//(2) Writing to a file
 fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
   console.log("File saved!");
 });
+
+//(3) Creating a web server
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8070);
